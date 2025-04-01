51d9e803ea754ed1abe5495a2b0d1c4421f383e4
unknown <s99644@pollub.edu.pl>
2025-04-01 19:27:15 +0200

    Koniec zajęć

--- app/src/main/java/com/example/app_2/PhoneViewModel.java
+++ app/src/main/java/com/example/app_2/PhoneViewModel.java
@@ -11,11 +11,19 @@ import com.example.app_2.model.PhoneRepository;
 import java.util.List;
 
 public class PhoneViewModel {
-    private PhoneRepository repository;
-    private LiveData<List<Phone>> allPhones;
+    private final PhoneRepository repository;
+    private final LiveData<List<Phone>> allPhones;
 
     public PhoneViewModel(@NonNull Application application) {
         this.repository = new PhoneRepository(application);
+        this.allPhones = this.repository.findAllPhones();
+    }
 
+    public LiveData<List<Phone>> getAllPhones(){
+        return this.allPhones;
+    }
+
+    public void deleteAllPhones(){
+        this.repository.deleteAllPones();
     }
 }
