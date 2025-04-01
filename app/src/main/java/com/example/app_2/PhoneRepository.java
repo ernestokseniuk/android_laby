1a5227cf2122d4fa83810b33eff6e1bd70bf82fe
unknown <s99644@pollub.edu.pl>
2025-04-01 19:17:41 +0200

    Next

--- app/src/main/java/com/example/app_2/PhoneRepository.java
+++ app/src/main/java/com/example/app_2/PhoneRepository.java
@@ -1,4 +1,28 @@
 package com.example.app_2;
 
+import android.app.Application;
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
 public class PhoneRepository {
+
+    private final PhoneDao phoneDao;
+    private final LiveData<List<Phone>> allPhones;
+
+    public PhoneRepository(Context context) {
+        this.phoneDao = PhoneRoomDatabase.getDatabase(context).phoneDao();
+        this.allPhones = this.phoneDao.findAllPhones();
+    }
+
+    public LiveData<List<Phone>> findAllPhones(){
+        return this.allPhones;
+    }
+
+    public void deleteAllPones(){
+        PhoneRoomDatabase.databaseWriteExecutor.execute(this.phoneDao::deleteAllPhones);
+    }
+
 }
